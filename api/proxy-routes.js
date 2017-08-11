462897d8876a7f023a42b0c4f37ed9323e5ca1bf
Hugo Gibson <gibsonhugo@gmail.com>
2017-08-11 16:50:09 +0100

    Make the routes way better

--- api/proxy-routes.js
+++ api/proxy-routes.js
@@ -1,16 +1,21 @@
 import proxy from "express-http-proxy";
 import passport from "passport";
+import url from "url";
 const ROOT = "http://46.101.241.38:8080";
+
+const options = {
+  proxyReqPathResolver: req => url.parse(req.url).pathname
+};
 
 export default function proxyRoutes(app) {
   app.post(
     "/api/encode",
     passport.authenticate("apikey-local", { failureRedirect: "/api" }),
-    proxy(ROOT)
+    proxy(ROOT, options)
   );
-  app.use(
+  app.get(
     "/api/status/poll/:id",
     passport.authenticate("apikey-local", { failureRedirect: "/api" }),
-    proxy(ROOT)
+    proxy(ROOT, options)
   );
 }
